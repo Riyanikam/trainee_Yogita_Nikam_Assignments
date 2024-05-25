ee7a343b26adfb0a5b022e62df58bcb0f3307100
Riyanikam <110924730+Riyanikam@users.noreply.github.com>
2024-05-25 23:58:45 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,279 +1,193 @@
+﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.ComponentModel.Design;
-using System.Net.Http.Headers;
-
-namespace ConsoleAppCURD
+using System.Xml;
+namespace ConsoleAppTask2
 {
     internal class Program
     {
-        class Task//Here we Create a class Task
-         {
-            public string Title { get; set; }
-            public string Description { get; set; }
-        }
-
-        static void Main(String[] args)
+       public class Item // Here we have created the class item
         {
-
-            List<Task> taskList = new List<Task>();//Created a list for the operation provide by the generic type  
-
-            bool exit = false;
-            while (!exit)
+            public int Id;
+            public string Name;
+            public int Price;
+            public int Quality;
+            public Item(int id,string name,int price,int quality)//Here we have created a constructor
             {
-                Console.WriteLine("List of the task,Enter your  choice");//Displaying a menu to Enter Choice
-                Console.WriteLine("1.Create a Task");
-                Console.WriteLine("2.Read a Task");
-                Console.WriteLine("3.Update a Task");
-                Console.WriteLine("4.Remove a Task");
-                Console.WriteLine("5.Exit");
-
-                int choice;
-                Console.Write("Enter a choice");
-
-                if (!int.TryParse(Console.ReadLine(), out choice))
+                Id=id;
+                Name=name;
+                Price=price;
+                Quality=quality;
+            }
+            public override string ToString()
+            {
+                return $"ID: {Id},Name:{Name},Price:{Price},Quality:{Quality}";
+            }
+        }
+        public class Inventory//Here we have created th class Inventory 
+        {
+            private List<Item>list;
+            public Inventory()
+            {
+                list=new List<Item>();
+            }
+            public void AddItem(int newId, string name, int price, int quality)// function for the Add the items to the list
+            {
+                int i = 0;
+                bool findDuplicated=false;
+                while (i<list.Count)
                 {
-                    Console.WriteLine("Invalid Input.Enter a number");
-                    continue;
+                    if (list[i].Id==newId)
+                    {
+                        Console.WriteLine("Item already exists");
+                        findDuplicated = true;
+                        break;
+                    }
+                    i++;
+                }
+                if (findDuplicated)
+                {
+                    return;
                 }
 
-                switch (choice)//Using Switch Case we have take a choice
+                Item newItem=new Item(newId,name,price,quality);
+                list.Add(newItem);
+                Console.WriteLine("Item added successfully");
+            }
+            public void DisplayItems()//function for the Display the items of the list
+            {
+                if (list.Count==0)
                 {
-                    case 1://Create operation has been perform here
-                        string Title;
-                        string Description;
-                        Console.WriteLine("Enter the task title");
-                        Title = Console.ReadLine();
-                        Console.WriteLine("Enter the task Description");
-                        Description = Console.ReadLine();
-                        Task newTask = new Task
-                        {
-                            Title = Title,
-                            Description = Description
-                        };
-                        taskList.Add(newTask);
-                        Console.WriteLine("Task is created");
-                        break;
+                    Console.WriteLine("Item list is empty");
+                    return;
+                }
 
-
-                    case 2://Here we have perform the Read operation
-                        if (taskList.Count == 0)
-                        {
-                            Console.WriteLine("No Available task");
-                        }
-                        else
-                        {
-                            Console.WriteLine("Task List \n ");
-                            foreach (var task in taskList)
-                            {
-                                Console.WriteLine($"Task: {task.Title} - {task.Description}");
-                            }
-
-                        }
-                        break;
-                
-                   //this Case is newly Updated
-
-                   case 3://by using the switch I have updated the task you cn only update the task also and description also and both the task and description 
-                        int TskToUpdate;
-                        int attribChoice;
-                        string newval;
-                        string newvaltwo;
-
-                        Console.WriteLine("Enter the task number you want to Update");
-                        TskToUpdate = int.Parse(Console.ReadLine()) - 1;
-
-                        if (TskToUpdate < 0 || TskToUpdate >= taskList.Count)
-                        {
-                            Console.WriteLine("The task Index is Invalid");
-                        }
-                        else
-                        {
-                            Console.WriteLine("Select the attribute to update");
-                            Console.WriteLine("1.Update Title");
-                            Console.WriteLine("2.Update Description");
-                            Console.WriteLine("3.Update Title and Description both");//Newly added choice
-
-                            Console.Write("Enter your Choice");
-                            attribChoice = int.Parse(Console.ReadLine());
-
-                            switch (attribChoice)
-                            {
-                                case 1://for Updating only title
-                                    Console.WriteLine("Enter a new value of the Title");
-                                    newval = Console.ReadLine();
-                                    taskList[TskToUpdate].Title = newval;
-                                    break;
-                                case 2://for Updating only description
-                                    Console.WriteLine("Enter a new value of the Description");
-                                    newval = Console.ReadLine();
-                                    taskList[TskToUpdate].Description = newval;
-                                    break;
-                                case 3://Here we can update both the thing 
-                                    Console.WriteLine("Enter a new value of the Title");
-                                    newval = Console.ReadLine();
-
-                                    Console.WriteLine("Enter a new value of the Description");
-                                    newvaltwo = Console.ReadLine();
-
-                                    taskList[TskToUpdate].Title = newval;
-                                    taskList[TskToUpdate].Description = newvaltwo;
-                                    break;
-
-                                    default:
-                                    Console.WriteLine("Invalid Choice\n");
-                                    break;
-                            }
-                            
-                        }
-                        break;
-
-
-                    case 4://deletion is done here remove the element of the specified position by giving the index  using list function and have check for the basic condition
-                        int indexTodel;
-
-                        if (taskList.Count == 0)
-                        {
-                            Console.WriteLine("No task is Available");
-                        }
-                        else
-                        {
-                            Console.WriteLine("Enter a Task index you want to delete");
-
-                            if (!int.TryParse(Console.ReadLine(), out indexTodel) || indexTodel < 1 || indexTodel > taskList.Count)
-                            {
-                                Console.WriteLine("Invalid Index.");
-                            }
-                            else
-                            {
-                                taskList.RemoveAt(indexTodel - 1);
-                                Console.WriteLine("Task deleted successfully");
-                            }
-                        }
-                        break;
-
-                    case 5://last case for remove ourself  from the program i.e exit
-                        exit = true;
-                        break;
-
-                    default://default case of the switch case whwn we enter the invalid choice which is not Present
-                        Console.WriteLine("Invalid Choice");
-                        break;
+                for (int i=0;i<list.Count;i++)
+                {
+                    Console.WriteLine(list[i]);
                 }
             }
+            public Item FindItemById(int id)//function for the FindItemById the items of the list
+            {
+                if (id<=0)
+                {
+                    Console.WriteLine("Enter a valid Id");
+                    return null;
+                }
+
+                for (int i=0;i<list.Count;i++)
+                {
+                    if (list[i].Id==id)
+                    {
+                        return list[i];
+                    }
+                }
+
+                Console.WriteLine("Item not found");
+                return null;
+            }
+            public void UpdateItem(int id,string name,int price,int quality)//function for updating all the the items of the list
+            {
+                if (id <= 0)
+                {
+                    Console.WriteLine("You have entered an invalid id. Please enter a valid id");
+                    return;
+                }
+                for (int i = 0; i < list.Count; i++)
+                {
+                    if (list[i].Id == id)
+                    {
+                        list[i].Name = name;
+                        list[i].Price = price;
+                        list[i].Quality = quality;
+                        Console.WriteLine("Item updated");
+                        return;
+                    }
+                }
+                Console.WriteLine("Item not found");
+            }
+            public void DeleteItem(int id)//function for deleting  the items from the list
+            {
+                for (int i=0;i<list.Count;i++)
+                {
+                    if (list[i].Id==id)
+                    {
+                        list.RemoveAt(i);
+                        Console.WriteLine("Item deleted");
+                        return;
+                    }
+                }
+                Console.WriteLine("Item not found");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Inventory In = new Inventory(); // Here we have created the object of the class and accessed it from its object name
+            Console.WriteLine("*******Items are added*******");
+            In.AddItem(1,"Maggi",20,10); // Added items
+            In.AddItem(2,"Biscuits",30,20); 
+            In.AddItem(3,"Chocolate",80,30); 
+            In.AddItem(4,"BournVita",90,10); 
+            In.DisplayItems(); // It will display all the items
+
+            //here we find the item by its id
+            Console.WriteLine("\n**********Found the  Item from ID***********");
+            Console.WriteLine("The 4 Item found by ID");
+            Item foundItem=In.FindItemById(4);
+            if (foundItem!=null)
+            {
+                Console.WriteLine($"Found Item: {foundItem}");
+            }
+
+            //here we Update the item
+            Console.WriteLine("\n**********Update all the Items**********");
+            In.UpdateItem(2,"Bread", 50, 17);
+            Console.WriteLine("The list After the Upation");
+            In.DisplayItems();
+
+            //here we have deleting the item
+            Console.WriteLine("\n**********deletion of a Items**********");
+            Console.WriteLine("The 3 item is delete");
+            In.DeleteItem(3);
+            Console.WriteLine("After deletion Items are");
+            In.DisplayItems();
         }
     }
 }
 
-
-/* OUTPUT OF THE CODE */
+/*****************OUTPUT********************/
 /*
-List of the task,Enter your  choice
-1.Create a Task
-2.Read a Task
-3.Update a Task
-4.Remove a Task
-5.Exit
-Enter a choice 1
-Enter the task title
-do code
-Enter the task Description
-do code in cpp
-Task is created
-List of the task,Enter your  choice
-1.Create a Task
-2.Read a Task
-3.Update a Task
-4.Remove a Task
-5.Exit
-Enter a choice 2
-Task List
+*******Items are added*******
+Item added successfully
+Item added successfully
+Item added successfully
+Item added successfully
+ID: 1,Name: Maggi,Price: 20,Quality: 10
+ID: 2,Name: Biscuits,Price: 30,Quality: 20
+ID: 3,Name: Chocolate,Price: 80,Quality: 30
+ID: 4,Name: BournVita,Price: 90,Quality: 10
 
-Task: do code - do code in cpp
-List of the task,Enter your  choice
-1.Create a Task
-2.Read a Task
-3.Update a Task
-4.Remove a Task
-5.Exit
-Enter a choice 3
-Enter the task number you want to Update
-1
-Select the attribute to update
-1.Update Title
-2.Update Description
-3.Update Title and Description both
-Enter your Choice 1
-Enter a new value of the Title
-write testcase
-List of the task,Enter your  choice
-1.Create a Task
-2.Read a Task
-3.Update a Task
-4.Remove a Task
-5.Exit
-Enter a choice 2
-Task List
+* *********Found the Item from ID***********
+The 4 Item found by ID
+Found Item: ID: 4,Name: BournVita,Price: 90,Quality: 10
 
-Task: write testcase - do code in cpp
-List of the task,Enter your  choice
-1.Create a Task
-2.Read a Task
-3.Update a Task
-4.Remove a Task
-5.Exit
-Enter a choice 3
-Enter the task number you want to Update
-1
-Select the attribute to update
-1.Update Title
-2.Update Description
-3.Update Title and Description both
-Enter your Choice 2
-Enter a new value of the Description
-make the testcase
-List of the task,Enter your  choice
-1.Create a Task
-2.Read a Task
-3.Update a Task
-4.Remove a Task
-5.Exit
-Enter a choice  2
-Task List
+* *********Update all the Items**********
+Item updated
+The list After the Upation
+ID: 1,Name: Maggi,Price: 20,Quality: 10
+ID: 2,Name: Bread,Price: 50,Quality: 17
+ID: 3,Name: Chocolate,Price: 80,Quality: 30
+ID: 4,Name: BournVita,Price: 90,Quality: 10
 
-Task: write testcase - make the testcase
-List of the task,Enter your  choice
-1.Create a Task
-2.Read a Task
-3.Update a Task
-4.Remove a Task
-5.Exit
-Enter a choice 3
-Enter the task number you want to Update
-1
-Select the attribute to update
-1.Update Title
-2.Update Description
-3.Update Title and Description both
-Enter your Choice 3
-Enter a new value of the Title
-coding
-Enter a new value of the Description
-code should be in java
-List of the task,Enter your  choice
-1.Create a Task
-2.Read a Task
-3.Update a Task
-4.Remove a Task
-5.Exit
-Enter a choice 2
-Task List
+* *********deletion of a Items**********
+The 3 item is delete
+Item deleted
+After deletion Items are
+ID: 1,Name: Maggi,Price: 20,Quality: 10
+ID: 2,Name: Bread,Price: 50,Quality: 17
+ID: 4,Name: BournVita,Price: 90,Quality: 10
 
-Task: coding  - code should be in java
-List of the task,Enter your  choice
-1.Create a Task
-2.Read a Task
-3.Update a Task
-4.Remove a Task
-5.Exit
 */
+
+
+
