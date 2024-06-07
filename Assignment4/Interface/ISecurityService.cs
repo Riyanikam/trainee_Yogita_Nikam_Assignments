98f29808a1f8081f22fa6b4202b20ac30e463cf8
Riyanikam <110924730+Riyanikam@users.noreply.github.com>
2024-06-06 22:34:43 -0700

    Add files via upload

---
+++ Assignment4/Interface/ISecurityService.cs
@@ -0,0 +1,15 @@
+﻿using VisitorSecuritySys.DTO;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace VisitorSecuritySys.Interface
+{
+    public interface ISecurityService
+    {
+        Task<SecurityDTO> AddSecurity(SecurityDTO securityDto);
+        Task<SecurityDTO> GetSecurityById(string id);
+        Task<IEnumerable<SecurityDTO>> GetAllSecurity();
+        Task<SecurityDTO> UpdateSecurity(SecurityDTO securityDto);
+        Task DeleteSecurity(string id);
+    }
+}
