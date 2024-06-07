6598447cc04ce3c6ffe6f9613cfe4c4f15fed1bb
Riyanikam <110924730+Riyanikam@users.noreply.github.com>
2024-06-06 22:22:44 -0700

    Add files via upload

---
+++ Assignment4/Entities/SecurityEntity.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+namespace VisitorSecuritySys.Entities
+{
+    public class SecurityEntity
+    {
+        [JsonProperty(PropertyName = "id")]
+        public string Id { get; set; }
+
+        [JsonProperty(PropertyName = "name")]
+        public string Name { get; set; }
+
+        [JsonProperty(PropertyName = "email")]
+        public string Email { get; set; }
+
+        [JsonProperty(PropertyName = "phone")]
+        public string Phone { get; set; }
+
+        [JsonProperty(PropertyName = "shift")]
+        public string Shift { get; set; }
+
+        [JsonProperty(PropertyName = "assignedLocation")]
+        public string AssignedLocation { get; set; }
+
+        [JsonProperty(PropertyName = "active")]
+        public bool Active { get; set; }
+
+
+    }
+}
