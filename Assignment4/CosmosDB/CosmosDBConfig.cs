1e4e3aaf230cab3fc090bbbec5a2b2242e8610b0
Riyanikam <110924730+Riyanikam@users.noreply.github.com>
2024-06-08 03:45:46 -0700

    Add files via upload

---
+++ Assignment4/CosmosDB/CosmosDBConfig.cs
@@ -0,0 +1,14 @@
+﻿namespace VisitorSecuritySys.CosmosDB
+{
+    namespace VisitorSecuritySys.CosmosDB
+    {
+        public class CosmosDBConfig
+        {
+            public static string CosmosEndPoint { get; set; }
+            public static string PrimaryKey { get; set; }
+            public static string DatabaseName { get; set; }
+            public static string ContainerName { get; set; }
+        }
+    }
+
+}
