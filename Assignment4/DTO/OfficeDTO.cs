97e1e59925c053369d2be65ad0ffd223358cf14b
Riyanikam <110924730+Riyanikam@users.noreply.github.com>
2024-06-08 03:47:09 -0700

    Add files via upload

---
+++ Assignment4/DTO/OfficeDTO.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+
+namespace VisitorSecuritySys.DTO
+{
+    public class OfficeDTO
+    {
+        [JsonProperty(PropertyName = "id", NullValueHandling = NullValueHandling.Ignore)]
+        public string Id { get; set; }
+
+        [JsonProperty(PropertyName = "name", NullValueHandling = NullValueHandling.Ignore)]
+        public string Name { get; set; }
+
+        [JsonProperty(PropertyName = "location", NullValueHandling = NullValueHandling.Ignore)]
+        public string Location { get; set; }
+
+    }
+}
