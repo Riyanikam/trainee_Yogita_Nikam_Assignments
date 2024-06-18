e56bf301ec5c4078b98e6e02e0950b3ee5644a17
Riyanikam <110924730+Riyanikam@users.noreply.github.com>
2024-06-18 12:55:17 +0530

    Add files via upload

---
+++ Assignment5/CosmosDB/ICosmosBDService.cs
@@ -0,0 +1,23 @@
+﻿using Assignmentfifth.Entity;
+
+namespace Assignmentfifth.CosmosDB
+{
+    public interface ICosmosDBService
+    {
+        Task<EmployeeBasicDetailsEntity> AddEmployee(EmployeeBasicDetailsEntity employeeBasicDetailsEntity);
+        Task<List<EmployeeBasicDetailsEntity>> GetAllEmployee();
+
+        Task<EmployeeBasicDetailsEntity> GetEmployeeByUId(string UId);
+
+        Task ReplaceAsync(dynamic employee);
+
+
+        Task<EmployeeAdditionalDetailsEntity> Add_AdditionalData(EmployeeAdditionalDetailsEntity employeeAdditionalDetailsEntity);
+
+
+        Task<List<EmployeeAdditionalDetailsEntity>> GetAllEmployeeAdditionalData();
+
+        Task<EmployeeAdditionalDetailsEntity> GetEmployeeAdditionalDataByUId(string uId);
+    }
+}
+
