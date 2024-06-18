f499a27998ce7f8d1d98f8689489a282c47b19a3
Riyanikam <110924730+Riyanikam@users.noreply.github.com>
2024-06-18 13:01:39 +0530

    Add files via upload

---
+++ Assignment5/Interface/IEmployeeAdditionalDetails.cs
@@ -0,0 +1,20 @@
+﻿using Assignmentfifth.DTO;
+
+namespace Assignmentfifth.Interface
+{
+    public class IEmployeeAdditonalDetail
+    {
+        public interface IEmployeeAdditionalDetails
+        {
+            Task<EmployeeAdditionalDetailsDTO> Add_AdditionalData(EmployeeAdditionalDetailsDTO employeeAdditinalDetailsDTO);
+            Task<List<EmployeeAdditionalDetailsDTO>> GetAllEmployeeAdditionalData();
+
+            Task<EmployeeAdditionalDetailsDTO> GetEmployeeAdditionalDataByUId(string uId);
+
+            Task<EmployeeAdditionalDetailsDTO> UpdateAdditionalData(EmployeeAdditionalDetailsDTO employeeAdditionalDetailsDTO);
+
+            Task<string> DeleteEmployeeAdditional(string UId);
+
+        }
+    }
+}
