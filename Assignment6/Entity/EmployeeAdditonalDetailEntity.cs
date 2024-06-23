0344c49002115c1c110ff302cda6cf19b7688bad
Riyanikam <110924730+Riyanikam@users.noreply.github.com>
2024-06-23 14:12:21 +0530

    Add files via upload

---
+++ Assignment6/Entity/EmployeeAdditonalDetailEntity.cs
@@ -0,0 +1,28 @@
+﻿using Assignmentfifth.Overall;
+using Newtonsoft.Json;
+
+namespace Assignmentfifth.Entity
+{
+    public class EmployeeAdditionalDetailsEntity : BaseEntity
+    {
+
+        [JsonProperty(PropertyName = "alternateEmail", NullValueHandling = NullValueHandling.Ignore)]
+        public string AlternateEmail { get; set; }
+
+
+        [JsonProperty(PropertyName = "alternateMobile", NullValueHandling = NullValueHandling.Ignore)]
+        public string AlternateMobile { get; set; }
+
+
+        [JsonProperty(PropertyName = "workInformation", NullValueHandling = NullValueHandling.Ignore)]
+        public WorkInfo WorkInformation { get; set; }
+
+
+        [JsonProperty(PropertyName = "personalDetails", NullValueHandling = NullValueHandling.Ignore)]
+        public PersonalDetail PersonalDetails { get; set; }
+
+
+        [JsonProperty(PropertyName = "identityInformation", NullValueHandling = NullValueHandling.Ignore)]
+        public IdentifyInfo IdentityInformation { get; set; }
+    }
+}
---
+++ Assignment6/Entity/IdentifyInfo.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+
+namespace Assignmentfifth.Entity
+{
+    public class IdentifyInfo
+    {
+        [JsonProperty(PropertyName = "pAN", NullValueHandling = NullValueHandling.Ignore)]
+        public string PAN { get; set; }
+
+        [JsonProperty(PropertyName = "aadhar", NullValueHandling = NullValueHandling.Ignore)]
+        public string Aadhar { get; set; }
+
+
+        [JsonProperty(PropertyName = "nationality", NullValueHandling = NullValueHandling.Ignore)]
+        public string Nationality { get; set; }
+
+
+        [JsonProperty(PropertyName = "passportNumber", NullValueHandling = NullValueHandling.Ignore)]
+        public string PassportNumber { get; set; }
+
+        [JsonProperty(PropertyName = "pFNumber", NullValueHandling = NullValueHandling.Ignore)]
+        public string PFNumber { get; set; }
+
+    }
+}
