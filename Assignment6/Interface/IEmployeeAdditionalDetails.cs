fce67ed7c491aeabf0e7818337e106cc8315bfb8
Riyanikam <110924730+Riyanikam@users.noreply.github.com>
2024-06-23 14:13:54 +0530

    Add files via upload

---
+++ Assignment6/Interface/IEmployeeAdditionalDetails.cs
@@ -0,0 +1,25 @@
+﻿using Assignmentfifth.DTO;
+
+namespace Assignmentfifth.Interface
+{
+    public class IEmployeeAdditonalDetail
+    {
+        public interface IEmployeeAdditionalDetails
+        {
+            Task<EmployeeAdditionalDetailsDTO> AddEmployeeByMakeRequest(EmployeeAdditionalDetailsDTO employeeAdditionalDetailsDTO);
+
+            Task<EmployeeAdditionalDetailsDTO> Add_AdditionalData(EmployeeAdditionalDetailsDTO employeeAdditinalDetailsDTO);
+
+            Task<List<EmployeeAdditionalDetailsDTO>> GetAllEmployeeAdditionalData();
+
+            Task<EmployeeAdditionalDetailsDTO> GetEmployeeByMakeGetRequest(EmployeeAdditionalDetailsDTO employeeAdditionalDetailsDTO);
+
+            Task<EmployeeAdditionalDetailsDTO> GetEmployeeAdditionalDataByUId(string uId);
+
+            Task<EmployeeAdditionalDetailsDTO> UpdateAdditionalData(EmployeeAdditionalDetailsDTO employeeAdditionalDetailsDTO);
+
+            Task<string> DeleteEmployeeAdditional(string UId);
+
+        }
+    }
+}
---
+++ Assignment6/Interface/IEmployeeBasicDetails.cs
@@ -0,0 +1,23 @@
+﻿using Assignmentfifth.DTO;
+
+namespace Assignmentfifth.Interface
+{
+    public interface IEmployeeBasicDetails
+    {
+
+        Task<EmployeeBasicDetailsDTO>AddEmployee(EmployeeBasicDetailsDTO employeeBasicDetailsDTO);
+
+        Task<List<EmployeeBasicDetailsDTO>> GetAllEmployee();
+
+        Task<EmployeeBasicDetailsDTO> GetEmployeeByUId(string UId);
+
+        Task<EmployeeBasicDetailsDTO> UpdateEmployee(EmployeeBasicDetailsDTO employeeBasicDetailsDTO);
+
+        Task<string> DeleteEmployee(string uId);
+
+        Task<List<EmployeeBasicDetailsDTO>> GetEmployeeByRole(string role);
+
+        Task<EmployeeFilter> GetEmployeebypagination(EmployeeFilter employeeFilter);
+
+    }
+}
