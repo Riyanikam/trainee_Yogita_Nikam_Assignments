ef9d69ba19d649f223f2a09b89fe79a6c22169da
Riyanikam <110924730+Riyanikam@users.noreply.github.com>
2024-06-23 14:15:51 +0530

    Add files via upload

---
+++ Assignment6/Overall/Main.cs
@@ -0,0 +1,12 @@
+﻿namespace Assignmentfifth.Overall
+{
+    public class Main
+    {
+        public static readonly string databaseName = Environment.GetEnvironmentVariable("databaseName");
+        public static readonly string containerName = Environment.GetEnvironmentVariable("containerName");
+        public static readonly string CosmosURL = Environment.GetEnvironmentVariable("cosmosUrl");
+        public static readonly string PrimaryKey = Environment.GetEnvironmentVariable("primaryKey");
+        public static readonly string EmployeeDocumentType = "Employee";
+
+    }
+}
