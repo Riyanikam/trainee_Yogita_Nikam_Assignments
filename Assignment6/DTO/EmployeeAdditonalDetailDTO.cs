c26569e027bb984417abd633f666a068c54591c3
Riyanikam <110924730+Riyanikam@users.noreply.github.com>
2024-06-23 14:11:13 +0530

    Add files via upload

---
+++ Assignment6/DTO/EmployeeAdditonalDetailDTO.cs
@@ -0,0 +1,29 @@
+﻿using Assignmentfifth.Entity;
+using Newtonsoft.Json;
+
+namespace Assignmentfifth.DTO
+{
+    public class EmployeeAdditionalDetailsDTO
+    {
+        [JsonProperty(PropertyName = "uId", NullValueHandling = NullValueHandling.Ignore)]
+        public string UId { get; set; }
+
+        [JsonProperty(PropertyName = "alternateEmail", NullValueHandling = NullValueHandling.Ignore)]
+        public string AlternateEmail { get; set; }
+
+        [JsonProperty(PropertyName = "alternateMobile", NullValueHandling = NullValueHandling.Ignore)]
+        public string AlternateMobile { get; set; }
+
+        [JsonProperty(PropertyName = "workInformation", NullValueHandling = NullValueHandling.Ignore)]
+        public WorkInfo WorkInformation { get; set; }
+
+        [JsonProperty(PropertyName = "personalDetails", NullValueHandling = NullValueHandling.Ignore)]
+        public PersonalDetail PersonalDetails { get; set; }
+
+        [JsonProperty(PropertyName = "identityInformation", NullValueHandling = NullValueHandling.Ignore)]
+        public IdentifyInfo IdentityInformation { get; set; }
+    }
+
+}
+
+
